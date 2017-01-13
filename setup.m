a7c957d94f838d007c00315a52c2499d5b773c0f
asadowns <asadowns@gmail.com>
2017-01-12 21:23:13 -0800

    Completed exercise 4

--- setup.m
+++ setup.m
@@ -5,8 +5,8 @@ whos  % list variables in workspace (detailed view)
 save hello.mat v;  % save variable v into file hello.mat
 save hello.txt v -ascii; % save as ascii
 % fopen, fread, fprintf, fscanf also work  [[not needed in class]]
- % To add the path for the current session of Octave:
-    addpath('/path/to/function/')
+% To add the path for the current session of Octave:
+addpath('/path/to/function/')
 
-    % To remember the path for future sessions of Octave, after executing addpath above, also do:
-    savepath
+% To remember the path for future sessions of Octave, after executing addpath above, also do:
+savepath
